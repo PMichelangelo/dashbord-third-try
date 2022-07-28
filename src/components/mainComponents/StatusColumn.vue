38c5d1c054b813751594042a8b3b484634c34108
Pmichelangelo <Pmichelangelo1377@gmail.com>
2022-07-28 21:04:34 +0300

    init

---
+++ src/components/mainComponents/StatusColumn.vue
@@ -0,0 +1,12 @@
+<script setup>
+const props = defineProps({
+  row: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <p>{{ props.row }}</p>
+</template>
--- src/components/mainComponents/TableMain.vue
+++ src/components/mainComponents/TableMain.vue
@@ -1,104 +1,116 @@
 <script setup>
-import tableIcon from "../../assets/icons/tableIcon.png"
+import tableIcon from "../../assets/icons/tableIcon.png";
+import UserColumn from "./UserColumn.vue";
+import StatusColumn from "./StatusColumn.vue";
 
-const columns = ['name', 'email', 'status', 'role']
+const columns = [
+  {
+    label: "name",
+    key: "name",
+    component: UserColumn,
+  },
+  {
+    label: "email",
+    key: "email",
+  },
+  {
+    label: "status",
+    key: "status",
+    component: StatusColumn,
+  },
+  {
+    label: "role",
+    key: "role",
+  },
+];
 
 const rows = [
   {
     name: "Roman Belic",
     email: "purchaseuikits@setproduct.com",
     status: "Free",
-    role: "Art Director 😎"
+    role: "Art Director 😎",
   },
   {
-
     name: "Sarah Smith",
     email: "sk8er@palacecrew.net",
     status: "Busy",
-    role: "UX Writer"
+    role: "UX Writer",
   },
   {
-
     name: "Eleanor Pena",
     email: "lookatmyhat@mail.me",
     status: "Working",
-    role: "UX Engineer"
+    role: "UX Engineer",
   },
   {
-
     name: "Rachel Richards",
     email: "rondondon@nfs.org",
     status: "Free",
-    role: "UI Master"
+    role: "UI Master",
   },
   {
-
     name: "Arlene McCoy",
     email: "arlenemccoyindagame@heyhey.com",
     status: "Working",
-    role: "Product Manager"
+    role: "Product Manager",
   },
   {
     name: "Courtney Henry",
     email: "henry96@mail.com",
     status: "Free",
-    role: "HR Assistant"
+    role: "HR Assistant",
   },
   {
-
     name: "Guy Hawkins",
     email: "thatguy@fromalaska.us",
     status: "Busy",
-    role: "The Boss"
+    role: "The Boss",
   },
   {
-
     name: "Devon Lane",
     email: "devon_dfq@eyefind.info",
     status: "Free",
-    role: "Full-Stack Developer"
+    role: "Full-Stack Developer",
   },
   {
-
     name: "Eleanor P. Junior",
     email: "epj2022@mail.com",
     status: "Working",
-    role: "UX Engineer"
+    role: "UX Engineer",
   },
   {
-
     name: "Maria Bruno",
     email: "bruno987@cast.co",
     status: "Busy",
-    role: "Manager"
+    role: "Manager",
   },
-]
+];
+const COLUMN_KEY = "status";
 </script>
-
 
 <template>
   <table>
     <thead>
       <tr>
         <th v-for="column in columns">
-          <span class="label">{{ column }}</span>
+          <span class="label">{{ column.label }}</span>
           <span class="icon-wrapper">
-            <span class="icon"><img src="../../assets/icons/up-arrow.svg" alt=""></span>
+            <span class="icon"
+              ><img src="../../assets/icons/up-arrow.svg" alt=""
+            /></span>
           </span>
         </th>
       </tr>
     </thead>
     <tbody>
-      <tr v-for="row in rows">
-        <td v-for="(value, field) in row">{{ value }} </td>
-        <div class="btn-wrapper">
-          <button>
-            <img src="../../assets/icons/pencil.svg" alt="edit">
-          </button>
-          <button>
-            <img src="../../assets/icons/trash.svg" alt="delete">
-          </button>
-        </div>
+      <tr v-for="row_item in rows">
+        <td v-for="column_item in columns">
+          <p v-if="column_item.component">
+            <component :is="column_item.component" :row="row_item" />
+          </p>
+          <p v-else>{{ row_item[column_item.key] }}</p>
+        </td>
       </tr>
     </tbody>
   </table>
@@ -108,7 +120,6 @@ const rows = [
 table {
   width: 100%;
   border-collapse: collapse;
-
 }
 
 thead th {
@@ -117,35 +128,34 @@ thead th {
   font-weight: 400;
   font-size: 14px;
   line-height: 20px;
-  color: #5A6474;
+  color: #5a6474;
   text-align: left;
 }
 
 tbody tr:nth-child(odd) {
-  background: #F8F8F8;
+  background: #f8f8f8;
 }
 
 tbody tr:nth-child(even) {
-  background: #FFFFFF;
+  background: #ffffff;
 }
 
 td {
   padding: 18px 22px;
   gap: 16px;
-  color: #5A6474;
+  color: #5a6474;
 }
 
 td:nth-child(1) {
   font-weight: 500;
   font-size: 18px;
   line-height: 24px;
-  color: #5A6474;
+  color: #5a6474;
 }
 
 .icon {
   margin-left: 8px;
 }
-
 
 .btn-wrapper {
   display: none;
